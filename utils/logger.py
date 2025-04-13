da0c6b9151ef63f61900406294bcc074d4016950
samgallic <samgallic@ufl.edu>
2025-04-12 23:10:31 -0400

    tiny fixes

--- utils/logger.py
+++ utils/logger.py
@@ -4,7 +4,6 @@ def log_weights(model):
     weights = {}
     # Calculate the mean of the square root of the squares of the weights
     for name, param in model.named_parameters():
-        print(name)
         if 'weight' in name or 'K_hit' in name or 'K_miss' in name:
             weights["weights/" + name + "_mean"] = torch.sqrt(param.data ** 2).mean().item()
 
